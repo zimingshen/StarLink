a3db7aeabb8041af541709fb10946faded64cd3a
zimingshen <hbshziming@gmail.com>
2022-08-23 11:08:39 -0700

    Finished

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../styles/App.css';
+
+import Header from "./Header";
+import Main from './Main';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <Main/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const SAT_API_KEY = "3T4RQM-CV6ZJ9-29RW4Y-4VQE";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
